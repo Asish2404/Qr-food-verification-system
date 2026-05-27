2febcd3cb91bcc696997724e7a5de420f7a71451
Aakash Howlader <aakashyk@gmail.com>
2026-05-27 21:50:16 +0530

    my changes

--- QR_code/src/components/QrScanner.jsx
+++ QR_code/src/components/QrScanner.jsx
@@ -1,8 +1,14 @@
+<<<<<<< Updated upstream
 import { useRef, useState } from "react";
 
 import { Html5Qrcode } from "html5-qrcode";
 
 import "./Qr.css";
+=======
+import React, { useEffect, useState } from "react";
+import { Html5QrcodeScanner } from "html5-qrcode";
+import "./QrScanner.css";
+>>>>>>> Stashed changes
 
 const QrScanner = () => {
 
@@ -110,6 +116,7 @@ const QrScanner = () => {
   };
   return (
     <div className="container">
+<<<<<<< Updated upstream
       <div className="left-panel">
         <h1 className="header">
           QR Scanner
@@ -166,6 +173,26 @@ const QrScanner = () => {
           </div>
         )}
       </div>
+=======
+      <h1 className="title">QR Scanner</h1>
+
+      <div id="reader"></div>
+
+      {scanResult && (
+        <div className="result-box">
+          <h3>Scanned Result:</h3>
+
+          <a
+            href={scanResult}
+            target="_blank"
+            rel="noreferrer"
+            className="result-link"
+          >
+            {scanResult}
+          </a>
+        </div>
+      )}
+>>>>>>> Stashed changes
     </div>
   );
 };
